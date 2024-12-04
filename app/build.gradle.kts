160b0a47b60050f51e7a103296210458a1358ae5
Oluwafemi <ojurifemi132@gmal.com>
2024-12-04 13:46:20 +0100

    chore: got rid of unused dependencies and updated target and compile SDKS

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,16 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.hilt)
-    alias(libs.plugins.ksp)
-    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
     namespace = "com.juri.lifelinedemo"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.juri.lifelinedemo"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -53,48 +50,21 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
-
     //fragment
     implementation("androidx.fragment:fragment-ktx:1.7.1")
 
     // coroutines for managing threads
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-
-    // retrofit for networking
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2")
-    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.9")
-
-    // moshi for parsing the JSON format
-    implementation("com.squareup.moshi:moshi:1.12.0")
-    implementation("com.squareup.moshi:moshi-kotlin:1.12.0")
-    ksp("com.squareup.moshi:moshi-kotlin-codegen:1.12.0")
-
     // Jetpack lifecycle components
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.1")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.1")
 
-    //Room
-    implementation("androidx.room:room-runtime:2.6.1")
-    ksp("androidx.room:room-compiler:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
-    testImplementation("androidx.room:room-testing:2.6.1")
-
-    implementation("androidx.multidex:multidex:2.0.1")
 
     //Material Design
     implementation("com.google.android.material:material:1.12.0")
 
-    //Hilt
-    implementation("com.google.dagger:hilt-android:2.49")
-    implementation("androidx.hilt:hilt-common:1.2.0")
-    implementation("androidx.hilt:hilt-work:1.2.0")
-    ksp("androidx.hilt:hilt-compiler:1.2.0")
-    ksp("com.google.dagger:hilt-android-compiler:2.49")
+
 
     //Timber
     implementation("com.jakewharton.timber:timber:5.0.1")
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,4 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-
-    id("com.google.gms.google-services") version "4.3.15" apply false
-    id("androidx.navigation.safeargs.kotlin") version "2.7.7" apply false
-    alias(libs.plugins.hilt) apply false
-    alias(libs.plugins.ksp) apply false
 }
